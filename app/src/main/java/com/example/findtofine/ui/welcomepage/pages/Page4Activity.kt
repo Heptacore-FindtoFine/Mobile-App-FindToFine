b1c392bea1392cd6fd8f1e5b20639d305f22505a
Muhammad Reza Ivano Pahlevi <rezaivano123@gmail.com>
2024-06-02 12:14:14 +0700

    Create Project

---
+++ app/src/main/java/com/example/findtofine/ui/welcomepage/pages/Page4Activity.kt
@@ -0,0 +1,12 @@
+package com.example.findtofine.ui.welcomepage.pages
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.findtofine.R
+
+class Page4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page4)
+    }
+}
---
+++ app/src/main/java/com/example/findtofine/ui/welcomepage/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.findtofine.ui.welcomepage.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.findtofine.databinding.ActivitySplashScreenBinding
+import com.example.findtofine.ui.welcomepage.pages.Page1Activity
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    private val splashTimeOut: Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,Page1Activity::class.java))
+            finish()
+        }, splashTimeOut)
+
+    }
+}
